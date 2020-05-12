14926dd4ef8675ac993de9b34c867a2e8ec12784
mars <shen227777777@163.com>
2020-05-12 17:32:53 +0800

    1.添加eslint 2.添加prettier 3.添加styleint

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'prettier',
+    // 专门支持了 eslint-plugin-react
+    'prettier/react',
+    // 专门支持了 @typescript-eslint/eslint-plugin
+    'prettier/@typescript-eslint'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2020,
+    sourceType: 'module'
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {}
+};
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # hello-boilerplate
 基于react typescript webpack的项目模板
+构建插件
+1.eslint
+2.stylelint
+3.prettier
